a221bc26197ac545f6bbe04bc869b5b8808f8332
zurc <cruzjurado@gmail.com>
2014-04-11 14:18:56 +0100

    new changes

---
+++ header.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<!--[if IE 8]> 				 <html class="no-js lt-ie9" lang="en" > <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en" > <!--<![endif]-->
+
+<head>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Naturaleza y bienestar</title>
+
+  <link href="http://fonts.googleapis.com/css?family=Quicksand:300,400,700|Oxygen:400,700,300" rel="stylesheet" type="text/css">
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+	
+  <script src="js/vendor/custom.modernizr.js"></script>
+  <link rel="stylesheet" href="css/foundation.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/general_foundicons.css">
+  <link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
+  
+</head>
+<body>
+
+<div class="container">
+	<div class="row wrap spadding">
+		<header>
+			<div class="small-6 large-4 columns headimg">
+				<a href="#">
+					<h3>Naturaleza y bienestar</h3>
+					<!-- <img src="img/head.png" alt="just"> -->
+				</a>
+			</div>
+			<div class="small-6 large-5 columns right header-info">
+				<div class="header-contact">					
+					<p><i class="foundicon-phone style1"></i> 914 444 444</p>
+					<p><i class="foundicon-mail style1"></i> info@just-espana.es</p> 
+				</div>
+			</div>			
+		</header>
+	</div>
+
+<div class="row ">
+	<div class="contain-to-grid sticky">
+		<nav class="top-bar">
+
+			<div class="rectangle-left"></div>
+				<div class="arrow-left"></div>
+				  <ul class="title-area">
+				    <!-- Title Area -->
+				    <li class="name">
+				      <h1><a href="index.php">Naturaleza y bienestar</a></h1>
+				    </li>
+				    <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+				    <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+				  </ul>
+				<div class="rectangle-right"></div>
+			<div class="arrow-right"></div>
+
+		  <section class="top-bar-section">
+		    <!-- Left Nav Section -->
+		    <ul class="left">
+		      <li class="divider"></li>
+				<li class="has-dropdown"><a href="#">PRODUCTOS</a>
+			      <ul class="dropdown">
+			          <li class="has-dropdown"><a href="farma-hierbas.php">Farmacia de hierbas</a>
+			            <ul class="dropdown">
+			              <li><label>Anti-celulitis</label></li>
+			              <li><a href="farma-hierbas.php#cr-arnica-raponchigo-incienso">Crema de Árnica, Rapónchigo e Incienso</a></li>
+			              <li><a href="farma-hierbas.php#cr-san-activ">Crema San Activ</a></li>
+			              <li><a href="farma-hierbas.php#cr-tea-tree-rosalina">Crema de Tea Tree y Rosalina</a></li>
+			              <li><a href="farma-hierbas.php#cr-calendula">Crema de Caléndula</a></li>
+			              <li><a href="farma-hierbas.php#cr-enebro">Crema de Enebro</a></li>
+			              <li><a href="farma-hierbas.php#cr-tomillo">Crema de Tomillo</a></li>
+			              <li><a href="farma-hierbas.php#gel-cr-consuelda">Gel cremoso de Consuelda</a></li>
+			              <li><a href="farma-hierbas.php#cr-manos-manzanilla">Crema de manos de Manzanilla</a></li>
+			              <li><a href="farma-hierbas.php#cr-lamelloderm">Crema de Lamelloderm</a></li>
+			              <li><a href="farma-hierbas.php#cr-lavanda">Crema de Lavanda</a></li>
+			              <li><a href="farma-hierbas.php#cr-cardamina">Crema de Cardamina</a></li>
+			              <li><a href="farma-hierbas.php#balsamo-alpes">Balsamo de los alpes</a></li>
+			            </ul>
+			          </li>
+			          <li class="has-dropdown"><a href="aceites-esenciales.php">Aceites Esenciales</a>
+						<ul class="dropdown">
+			              <li><label>Aceites Esenciales</label></li>
+			              <li><a href="aceites-esenciales.php#lavanda-natural">Lavanda Natural</a></li>
+			              <li><a href="aceites-esenciales.php#naranja-natural">Naranja Natural</a></li>
+			              <li><a href="aceites-esenciales.php#tea-tree">Tea Tree con Manuca Rosalina</a></li>
+			              <li><a href="aceites-esenciales.php#limon-natural">Limon Natural</a></li>
+			            </ul>
+			          </li>
+			          <li class="has-dropdown"><a href="bano-ducha.php">Baño y ducha</a>
+						<ul class="dropdown">
+			              <li><label>Baño y ducha</label></li>
+			              <li><a href="bano-ducha.php#cepillo-masajes">Cepillo para masaje</a></li>
+			              <li><a href="bano-ducha.php#esponja-nylon">Esponja de nylon</a></li>
+			              <li><a href="bano-ducha.php#esencia-tomillo">Esencia de baño de tomillo</a>
+			              <li><a href="bano-ducha.php#esencia-sandalo">Esencia para baño de sándalo</a></li>
+			              <li><a href="bano-ducha.php#alpes">Baño de los alpes</a></li>
+			              <li><a href="bano-ducha.php#relajacion">Baño de relajación</a></li>
+			              <li><a href="bano-ducha.php#ducha-miel-y-arroz">Ducha de miel y arroz</a></li>
+			              <li><a href="bano-ducha.php#sales-mar-muerto">Sales de baño san activ con sales del mar muerto</a></li>
+			              <li><a href="bano-ducha.php#locion-ducha">Deo intim - Loción de ducha</a>		              
+			              <li><a href="bano-ducha.php#ducha-lamelloderm">Ducha de lamelloderm</a>		              
+			            </ul>
+			          </li>
+			          <li class="has-dropdown"><a href="linea-edelweiss.php">línea Edelweiss</a>
+			            <ul class="dropdown">
+			              <li><label>linea-edelweiss</label></li>
+			              <li><a href="linea-edelweiss.php#1">1</a></li>
+			              <li><a href="linea-edelweiss.php#2">2</a></li>
+			              <li><a href="linea-edelweiss.php#3">3</a></li>
+			              <li><a href="linea-edelweiss.php#4">4</a></li>
+			            </ul>
+			          </li>
+			          <li class="has-dropdown"><a href="cuidado-de-pies.php">Cuidado de pies</a>
+			            <ul class="dropdown">
+			              <li><label>cuidado-de-pies</label></li>
+			              <li><a href="cuidado-de-pies.php#1">1</a></li>
+			              <li><a href="cuidado-de-pies.php#2">2</a></li>
+			              <li><a href="cuidado-de-pies.php#3">3</a></li>
+			              <li><a href="cuidado-de-pies.php#4">4</a></li>
+			            </ul>
+			          </li>
+			          <li class="has-dropdown"><a href="cuidado-facial.php">Cuidado Facial</a>
+			            <ul class="dropdown">
+			              <li><label>cuidado-facial</label></li>
+			              <li><a href="cuidado-facial.php#1">1</a></li>
+			              <li><a href="cuidado-facial.php#2">2</a></li>
+			              <li><a href="cuidado-facial.php#3">3</a></li>
+			              <li><a href="cuidado-facial.php#4">4</a></li>
+			              <li><a href="cuidado-facial.php#5">5</a></li>
+			              <li><a href="cuidado-facial.php#6">6</a></li>
+			              <li><a href="cuidado-facial.php#7">7</a></li>
+			              <li><a href="cuidado-facial.php#8">8</a></li>
+			              <li><a href="cuidado-facial.php#9">9</a></li>
+			              <li><a href="cuidado-facial.php#10">10</a></li>
+			            </ul>
+			          </li>
+			          <li class="has-dropdown"><a href="cuidado-del-cabello.php">Cuidado del Cabello</a>
+			            <ul class="dropdown">
+			              <li><label>cuidado-del-cabello</label></li>
+			              <li><a href="cuidado-del-cabello.php#1">1</a></li>
+			              <li><a href="cuidado-del-cabello.php#2">2</a></li>
+			              <li><a href="cuidado-del-cabello.php#3">3</a></li>
+			              <li><a href="cuidado-del-cabello.php#4">4</a></li>
+			              <li><a href="cuidado-del-cabello.php#5">5</a></li>
+			              <li><a href="cuidado-del-cabello.php#6">6</a></li>
+			              <li><a href="cuidado-del-cabello.php#7">7</a></li>
+			            </ul>
+			          </li>
+			          <li class="has-dropdown"><a href="productos-para-el-hogar.php">Productos para el Hogar</a>
+			            <ul class="dropdown">
+			              <li><label>productos-para-el-hogar</label></li>
+			              <li><a href="productos-para-el-hogar.php#1">1</a></li>
+			              <li><a href="productos-para-el-hogar.php#2">2</a></li>
+			            </ul>
+			          </li>
+			          <li class="has-dropdown"><a href="solo-para-hombres.php">Sólo para Hombres</a>
+			            <ul class="dropdown">
+			              <li><label>solo-para-hombres</label></li>
+			              <li><a href="solo-para-hombres.php#1">1</a></li>
+			              <li><a href="solo-para-hombres.php#2">2</a></li>
+			              <li><a href="solo-para-hombres.php#3">3</a></li>
+			            </ul>
+			          </li>
+			          <li class="has-dropdown"><a href="cuidado-del-cuerpo.php">Cuidado del Cuerpo</a>
+			            <ul class="dropdown">
+			              <li><label>cuidado-del-cuerpo</label></li>
+			              <li><a href="cuidado-del-cuerpo.php#1">1</a></li>
+			              <li><a href="cuidado-del-cuerpo.php#2">2</a></li>
+			              <li><a href="cuidado-del-cuerpo.php#3">3</a></li>
+			              <li><a href="cuidado-del-cuerpo.php#4">4</a></li>
+			              <li><a href="cuidado-del-cuerpo.php#5">5</a></li>
+			              <li><a href="cuidado-del-cuerpo.php#6">6</a></li>
+			            </ul>
+			          </li>
+			          <li class="has-dropdown"><a href="herboristeria.php">Herboristería</a>
+			            <ul class="dropdown">
+			              <li><label>herboristeria</label></li>
+			              <li><a href="herboristeria.php#1">1</a></li>
+			              <li><a href="herboristeria.php#2">2</a></li>
+			              <li><a href="herboristeria.php#3">3</a></li>
+			              <li><a href="herboristeria.php#4">4</a></li>
+			              <li><a href="herboristeria.php#5">5</a></li>
+			              <li><a href="herboristeria.php#6">6</a></li>
+			              <li><a href="herboristeria.php#7">7</a></li>
+			              <li><a href="herboristeria.php#8">8</a></li>
+			              <li><a href="herboristeria.php#9">9</a></li>
+			              <li><a href="herboristeria.php#10">10</a></li>
+			              <li><a href="herboristeria.php#11">11</a></li>
+			              <li><a href="herboristeria.php#12">12</a></li>
+			              <li><a href="herboristeria.php#13">13</a></li>
+			            </ul>
+			          </li>
+			          <li class="has-dropdown"><a href="linea-lamelloderm.php">Línea Lamelloderm</a>
+			            <ul class="dropdown">
+			              <li><label>linea-lamelloderm</label></li>
+			              <li><a href="linea-lamelloderm.php#1">1</a></li>
+			              <li><a href="linea-lamelloderm.php#2">2</a></li>
+			              <li><a href="linea-lamelloderm.php#3">3</a></li>
+			            </ul>
+			          </li>
+			          <li class="has-dropdown"><a href="linea-malven.php">Línea Malven</a>
+			            <ul class="dropdown">
+			              <li><label>linea-malven</label></li>
+			              <li><a href="linea-malven.php#1">1</a></li>
+			              <li><a href="linea-malven.php#2">2</a></li>
+			              <li><a href="linea-malven.php#3">3</a></li>
+			              <li><a href="linea-malven.php#4">4</a></li>
+			            </ul>
+			          </li>
+			          <li class="has-dropdown"><a href="nuevo.php">Nuevo</a>
+			            <ul class="dropdown">
+			              <li><label>nuevo</label></li>
+			              <li><a href="nuevo.php#1">1</a></li>
+			            </ul>
+			          </li>
+			          <li class="has-dropdown"><a href="cuidado-solar.php">Cuidado Solar</a>
+			            <ul class="dropdown">
+			              <li><label>cuidado-solar</label></li>
+			              <li><a href="cuidado-solar.php#1">1</a></li>
+			              <li><a href="cuidado-solar.php#2">2</a></li>
+			              <li><a href="cuidado-solar.php#3">3</a></li>
+			              <li><a href="cuidado-solar.php#4">4</a></li>
+			            </ul>
+			          </li>
+			        </ul>
+				</li>
+		      <li class="divider"></li>
+		    </ul>
+
+		    <!-- Right Nav Section -->
+		    <ul class="right">
+		      <li class="divider hide-for-small"></li>
+		      <li><a href="#">CONTACTO</a></li>
+		      <li class="divider"></li>
+		      <li class="has-form">
+		        <form>
+		          <div class="row collapse">
+		            <div class="small-8 columns">
+		              <input type="text">
+		            </div>
+		            <div class="small-4 columns">
+		              <a href="#" class="button">Busca</a>
+		            </div>
+		          </div>
+		        </form>
+		      </li>
+		    </ul>
+		  </section>
+		</nav>
+	</div>
+</div>
